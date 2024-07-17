72fbbb95d80422908ca2c997857a94d67f13d164
Fardeen S <fardeenclan@gmail.com>
2024-07-17 19:23:43 +0530

    final

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { Container, Stack } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
-
+export const BASE_URL = "http://localhost:4000/api";
 function App() {
   return (
     <Stack h={"100vh"}>
